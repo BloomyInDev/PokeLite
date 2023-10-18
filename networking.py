3cb854caeeab7f7f9fa46db3776b3e0de618db95
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2023-10-18 21:58:32 +0200

    Starting to make networking
    - Starting to make networking

---
+++ networking.py
@@ -0,0 +1,15 @@
+import uuid, os
+data:bytes=uuid.uuid4().bytes
+if os.path.isfile('./data'):
+    with open('./data','rb') as file:
+        data = file.read(16)
+else:
+    with open('./data','wb') as file:
+        file.write(uuid.uuid4().bytes)
+print(data)
+class Networking:
+    def __init__(self,pseudo:str) -> None:
+        assert(isinstance(pseudo,str))
+        self.pseudo:str = pseudo
+        self.__uuid = uuid.uuid4()
+        pass
